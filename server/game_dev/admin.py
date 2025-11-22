47d8de8338ceae80177d8bb9e37111709151437d
James Lee <jameslee6352@gmail.com>
2025-11-22 15:28:27 +0800

    Registering member models

--- server/game_dev/admin.py
+++ server/game_dev/admin.py
@@ -1,3 +1,9 @@
-# from django.contrib import admin
+from django.contrib import admin
+from .models import Member
 
-# Register your models here.
+
+class MemberAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Member, MemberAdmin)
